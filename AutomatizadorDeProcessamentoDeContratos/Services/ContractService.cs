9767d1f989bc326d1b16360c557d34f2d3d2445e
Vinicius Passos <vinicius.pmp19@gmail.com>
2022-10-07 15:44:33 -0300

    Projeto para automatização de processo de contratos

---
+++ AutomatizadorDeProcessamentoDeContratos/Services/ContractService.cs
@@ -0,0 +1,33 @@
+﻿using AutomatizadorDeProcessamentoDeContratos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatizadorDeProcessamentoDeContratos.Services
+{
+    class ContractService
+    {
+
+        private IOnlinePaymentService _onlinePaymentService;
+
+        public ContractService(IOnlinePaymentService onlinePaymentService)
+        {
+            _onlinePaymentService = onlinePaymentService;
+        }
+
+        public void ProcessContract(Contract contract, int months)
+        {
+            double basicQuota = contract.TotalValue / months; // divide o valor total do contrato pelos meses
+            for (int i = 1; i <= months; i++)
+            {
+                DateTime date = contract.Date.AddMonths(i);
+                double updatedQuota = basicQuota + _onlinePaymentService.Interest(basicQuota, i); //200 + 1% * 1
+                double fullQuota = updatedQuota + _onlinePaymentService.PaymentFee(updatedQuota); // 200 + 2%
+                contract.AddInstallment(new Installment(date, fullQuota)); // instanciamento
+            }
+        }
+    }
+}
+
